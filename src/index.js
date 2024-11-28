605f8cbef8ad6912836ec032be81cf2c199d4766
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2024-11-28 13:17:08 +0530

    feat: basic code added

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css"; // Optional: If you plan to use CSS
+
+const App = () => <h1>Hello, React on Node.js!</h1>;
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true, // Clean the dist folder before each build
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"], // Automatically resolve file extensions
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html", // Use a custom HTML template
+    }),
+  ],
+  mode: "development",
+};
