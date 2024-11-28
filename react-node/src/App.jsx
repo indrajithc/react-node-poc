608e71d1d56480fe07123bc01052a9f27bcb2821
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2024-11-28 23:32:31 +0530

    refactor: moved to app

---
+++ react-node/src/App.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const App = () => <h1>Hello, React new date on Node.js!</h1>;
+
+export default App;
--- react-node/src/index.js
+++ react-node/src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css"; // Optional: If you plan to use CSS
 
-const App = () => <h1>Hello, React on Node.js!</h1>;
+import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
