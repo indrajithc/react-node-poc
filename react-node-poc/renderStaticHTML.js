f5255867b2787c54429b7f2a51b7c1124db7589f
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2025-03-09 14:01:50 +0530

    feat: new react poc for split bundle

---
+++ react-node-poc/renderStaticHTML.js
@@ -0,0 +1,35 @@
+require("@babel/register")({
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+});
+const fs = require("fs");
+const path = require("path");
+const React = require("react");
+const ReactDOMServer = require("react-dom/server");
+const App = require("./src/App").default; // Ensure App is exported as default
+
+
+
+const handler =  () => new Promise((resolve, reject) => {
+  
+  
+    fetch("https://reqres.in/api/users?page=2").then((response) => {
+      return response.json();
+    }).then((data) => {
+      console.log({
+
+        list : data.data
+      });
+
+      const outputHtml = React.createElement(App, { list: data.data });
+
+      resolve(ReactDOMServer.renderToStaticMarkup(outputHtml));
+    }
+    ).catch((error) => {
+      console.error("Error fetching data:", error);
+      reject(error);
+    });
+  } );
+
+
+
+module.exports = handler;
---
+++ react-node-poc/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css"; // Optional: If you plan to use CSS
+
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
