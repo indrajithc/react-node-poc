b8e9198395f0121a72a7f69d2e483432f251e7ac
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2025-03-09 19:25:30 +0530

    refactor: code split

--- react-node-experimental-v01/webpack.config.js
+++ react-node-experimental-v01/webpack.config.js
@@ -2,19 +2,18 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-
 module.exports = async () => {
-  
-  const {
-    html: htmlContent,
-    props,
-  } = await require("./renderStaticHTML.js")();
+  const { html: htmlContent, props } = await require("./renderStaticHTML.js")();
 
-  return ( {
-    entry: "./src/index.js",
+  return {
+    entry: {
+      root: ["react", "react-dom"], // React-specific JS (like jQuery)
+      app: "./src/index.js", // Main application logic
+    },
     output: {
       path: path.resolve(process.cwd(), "dist"),
-      filename: "js/bundle.js",
+      filename: "js/[name].js", // Fixed name for root.js
+      chunkFilename: "js/[name].[contenthash].js",
       clean: true,
     },
     module: {
@@ -33,9 +32,22 @@ module.exports = async () => {
     resolve: {
       extensions: [".js", ".jsx"],
     },
+    optimization: {
+      splitChunks: {
+        chunks: "all",
+        cacheGroups: {
+          vendors: {
+            test: /[\\/]node_modules[\\/]/, // Other node_modules dependencies
+            name: "vendors",
+            chunks: "all",
+            enforce: true,
+          },
+        },
+      },
+    },
     plugins: [
       new MiniCssExtractPlugin({
-        filename: "css/styles.css", // Extract CSS into css/styles.css file
+        filename: "css/styles.css",
       }),
       new HtmlWebpackPlugin({
         title: "React Node.js POC",
@@ -46,20 +58,22 @@ module.exports = async () => {
               <meta charset="UTF-8">
               <meta name="viewport" content="width=device-width, initial-scale=1.0">
               <title>React Node.js POC</title>
-              <link rel="stylesheet" href="css/styles.css"> <!-- Link to extracted CSS -->
-              </head>
-              <body>
+              <link rel="stylesheet" href="css/styles.css">
+            </head>
+            <body>
               <div id="root">${htmlContent}</div>
-               <script type="text/javascript">
+              <script type="text/javascript">
                 window.__REACT_PROPS__ = JSON.parse(\`${JSON.stringify(props)}\`);
               </script>
-              <script defer src="js/bundle.js"></script> <!-- Link to extracted JS -->
-              </body>
+              <script async src="js/vendors.js"></script> <!-- Other dependencies -->
+              <script async src="js/root.js"></script> <!-- Load React first -->
+              <script defer src="js/app.js"></script> <!-- App logic -->
+            </body>
           </html>
         `,
-        inject: false, // Prevents Webpack from auto-injecting CSS/JS
+        inject: false,
       }),
     ],
     mode: "production",
-  });
-}
+  };
+};
